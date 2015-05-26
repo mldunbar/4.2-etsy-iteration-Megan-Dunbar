6acc53b1a69491b312a0a3ccb5eb8f2ab74a7a87
Megan Dunbar <meganleedunbar@gmail.com>
2015-05-26 15:00:09 -0400

    everything linked, 2 problems attempted

---
+++ main.js
@@ -0,0 +1,31 @@
+var _ = require('underscore');
+var data = require('./items.json');
+
+//Getting prices from the data.
+
+// var prices = _.pluck (data, 'price');
+
+//Reducing the prices to a single number
+//Dividing that by the number of items in the array
+
+// var avgPrice = _.reduce(prices, function(a, b);
+//
+// console.log(prices)
+
+//Second question!
+
+//get prices between $14 and $18
+//list of prices is already plucked
+
+// var priceRange = _.filter(prices,
+//   function(priceList){
+//     if _.each (prices) >= 14 && <=18;
+//     return true;
+//     }
+//     else {
+//       return false;
+//     }
+//     );
+// console.log(priceRange)
+
+//get names of said items
